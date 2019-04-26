e0ef73b44415e03a8a259f7ff605134af9b496ba
noble0423 <ryan.noble82@gmail.com>
2019-04-25 19:05:41 -0500

    toggled the modal back on for the deployed site while I work on a cors error.

--- assets/php/contactUsEmail.php
+++ assets/php/contactUsEmail.php
@@ -1,29 +1,29 @@
 <?php
     // header("Access-Control-Allow-Origin: *");
-	// Contact
-	$to = "ryan.noble82@gmail.com";
-	// $subject = 'Subject here...';
+	// // Contact
+	// $to = "ryan.noble82@gmail.com";
+	// // $subject = 'Subject here...';
 
-	if(isset($_POST["c_firstname"]) && isset($_POST["c_lastname"]) && isset($_POST["c_email"]) && isset($_POST["c_subject"]) && isset($_POST["c_message"])){
-        $firstname  = $_POST["c_firstname"];
-        $lastname   = $_POST["c_lastname"];
-		$from       = $_POST["c_email"];
-		$subject    = $_POST["c_subject"];
-		$message    = $_POST["c_message"];
+	// if(isset($_POST["c_firstname"]) && isset($_POST["c_lastname"]) && isset($_POST["c_email"]) && isset($_POST["c_subject"]) && isset($_POST["c_message"])){
+    //     $firstname  = $_POST["c_firstname"];
+    //     $lastname   = $_POST["c_lastname"];
+	// 	$from       = $_POST["c_email"];
+	// 	$subject    = $_POST["c_subject"];
+	// 	$message    = $_POST["c_message"];
 
-		if (mail($to, $firstname, $lastname, $subject, $message, $from)) {
-			$result = array(
-				"message" => "Thanks for contacting me!",
-				"sendstatus" => 1
-				);
-			echo json_encode($result);
-		} else {
-			$result = array(
-				"message" => "Sorry, something is wrong",
-				"sendstatus" => 1
-				);
-			echo json_encode($result);
-		}
-	}
+	// 	if (mail($to, $firstname, $lastname, $subject, $message, $from)) {
+	// 		$result = array(
+	// 			"message" => "Thanks for contacting me!",
+	// 			"sendstatus" => 1
+	// 			);
+	// 		echo json_encode($result);
+	// 	} else {
+	// 		$result = array(
+	// 			"message" => "Sorry, something is wrong",
+	// 			"sendstatus" => 1
+	// 			);
+	// 		echo json_encode($result);
+	// 	}
+	// }
 
 ?>
