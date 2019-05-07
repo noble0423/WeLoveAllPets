bb636a45231ca5e3aa944aba914224cc189fa598
noble0423 <ryan.noble82@gmail.com>
2019-05-07 12:40:34 -0500

    Finished styling the Svcs Section (changed fonts, added text-shadow to make text pop incl inside the modals, added icon borders, transpaernt background).

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,15 @@
 // Scroll Effect
 
+// window.addEventListener("resize", function() {
+//     if (window.matchMedia("(min-width: 1200px)").matches) {
+//         console.log("Screen width is at least 1200px");
+//     } else {
+//         console.log("Screen less than 1200px");
+//     }
+// });
+
 $(window).scroll(function() {
-    $("nav").toggleClass("scrolled", $(this).scrollTop() > 855); 
-    $(".dropdown-menu").toggleClass("scrolled", $(this).scrollTop() > 855);    
+    $("nav").toggleClass("scrolled", $(this).scrollTop() > 450); 
+    // $(".dropdown-menu").toggleClass("scrolled", $(this).scrollTop() > 855);    
 });
 
