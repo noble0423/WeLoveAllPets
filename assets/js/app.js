9bf6374019cf24e4fc86efdb5f1c65de86be9b03
noble0423 <ryan.noble82@gmail.com>
2019-06-05 15:06:18 -0500

    Finished the Book Now Section -- added slight animation (Square Widget grows as it opens). QA'd the Book Now Section for responsieness.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
     $("#book-now-btn").click(function(){
+        // console.log("clicked");
+        // $(".square-widget").fadeToggle();
         $(".square-widget").toggleClass("square-widget-visible");
     });
 });
