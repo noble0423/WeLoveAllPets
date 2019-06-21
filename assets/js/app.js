75ecadf311ce45de53af9db293ed0be5bda26e28
noble0423 <ryan.noble82@gmail.com>
2019-06-21 10:58:50 -0500

    added an array of Customer Review objects in order to cycle through. tested with a for loop and info is displaying in the console correctly.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,84 @@
+// VARIABLES
+// =================================================================================================
+const customerReviewCard = $("#customer-review-one");
+const countStartNum = 15;
+
+const reviewsArray = [{
+    review: 'We’re really happy that we found We Love All Pets! We had never used a pet sitting service before but have used them multiple times now, and will definitely be using them again.',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 2.png',
+    name: 'Julie and Brad T.',
+    area: ' from The Med Center'
+}, {
+    review: 'The Best The Best The Best!!! I’ve used Gabe for over 7 years now. I am blessed to have found him and his team, as they have always treated my dog Jazz with nothing but the best in love and care.',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 1.png',
+    name: 'Gwinny T.',
+    area: ' from The Galleria'
+}, {
+    review: '...there was never a moment of concern or worry. The updates I received put my mind at ease, and they made sure to maintain that contact regularly.',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 2.png',
+    name: 'Michelle D.',
+    area: ' from River Oaks'
+}, {
+    review: 'You can’t go wrong with the team at We Love All Pets. Without a doubt, it is apparent that their primary concern is the wellbeing of your pet.  Our two Danes, Loki and Thor, absolutely love them.',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Man 2.png',
+    name: 'James and Lily F.',
+    area: ' from Rice Village'
+}, {
+    review: 'We Love All Pets has been a Lifesaver time and time again. We’ve used Gabriel’s and his team for a few years now and have never been worried about our pup, Cheerio, while in their care.',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 1.png',
+    name: 'Laura T.',
+    area: ' West University Place'
+}, {
+    review: 'As a nervous dog mom of a high-needs rescue dog, I always feel safe with them watching and caring for Murray.',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 2.png',
+    name: 'Jamie A.',
+    area: ' from The Heights'
+}, {
+    review: 'Our Frenchie has had multiple spine surgeries for intervertebral disk disease, so it is always difficult leaving her under the care of someone else as she is very fragile. Gabriel and his team have been absolutely wonderful with our baby...',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Man 1.png',
+    name: 'Marc E.',
+    area: ' from Museum District'
+}, {
+    review: 'Gabe and his team at We Love All Pets are the best pet sitters you could ask for. They are reliable and trustworthy and they truly care for animals (and their owners) ...',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 1.png',
+    name: 'Catherine W.',
+    area: ' from The Museum District'
+}, {
+    review: `My husband and I seriously lucked out when we found Gabe with We Love All Pets! Not only is he the nicest guy (literally THE NICEST), he is extremely knowledgeable about dogs (and I'm sure cats too) and every dog seems to love him.`,
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 2.png',
+    name: 'Kelly B.D.',
+    area: ' from Museum District'
+}, {
+    review: 'Gabriel Kelley has built his company, We Love All Pets into an organization with a solid reputation for premium service. He and his personnel provide a rich giving atmosphere for our pets anytime we need them ...',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Man 1.png',
+    name: 'John K.',
+    area: ' from The Heights'
+}, {
+    review: 'After our former dog sitter moved away we were at a loss for someone to tend to our 17-year-old dog who is on meds and a finicky eater. At the suggestion of our vet we tried We Love All Pets. Gabriel and his crew are kind and patient with our pup and they assure that she has all of her meds and eats while we are away. We are so thankful for our vet’s recommendation.',
+    icon: './assets/images/Updated Logo and Thumbnail Icons/Icons copy/png/Woman 1.png',
+    name: 'Diane E.',
+    area: ' from Museum District'
+}];
+
+
+
+
+// APP LOGIC
+// =================================================================================================
 $(document).ready(function(){
 
+    // Customer Reviews - Scrolling
+    // Testing the Array of Objects w/ a for loop
+    for (let i = 0; i < reviewsArray.length; i++) {
+        console.log(`review ${i}: ${reviewsArray[i].review}`);
+        console.log(`name ${i}: ${reviewsArray[i].name}`);
+        console.log(`area ${i}: ${reviewsArray[i].area}`);
+        console.log("**************************************************************************");
+    }
+
+    // 
+
+    // Square Widget Visibility
     $("#book-now-btn").click(function(){
         // console.log("clicked");
         // $(".square-widget").fadeToggle();
