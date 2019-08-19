3f69dbff0de17e88e27a640ca86278fb0e0c662d
noble0423 <ryan.noble82@gmail.com>
2019-08-19 13:00:16 -0500

    trying to add a custom from email address to use instead of the generic ip address info.

--- assets/php/contactUsEmail.php
+++ assets/php/contactUsEmail.php
@@ -1,15 +1,16 @@
 <?php
     // header("Access-Control-Allow-Origin: *");
 	// Contact
-	$to = "ryan.noble82@gmail.com";
+	$to 	= "ryan.noble82@gmail.com";
+	$from	= "website_inquiry@weloveallpets.com";
 
 	if(isset($_POST["c_fullname"]) && isset($_POST["c_emailreplyto"]) && isset($_POST["c_subject"]) && isset($_POST["c_message"])){
 		$fullname   = $_POST["c_fullname"];
-		$from       = $_POST["c_emailreplyto"];
+		$useremail  = $_POST["c_emailreplyto"];
 		$subject    = $_POST["c_subject"];
 		$message    = $_POST["c_message"];
 
-		if (mail($to, $subject, $message, $from)) {
+		if (mail($to, $subject, $message, $useremail, $from)) {
 			$result = array(
 				"message" => "Thank you very much for your email! One of our friendly staff members will reach out to you shortly.",
 				"sendstatus" => 1
